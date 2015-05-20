3033a27f89088da18a0ea5064ff4a34fb95ac0ff
Christoph Dorn <christoph@christophdorn.com>
2015-05-20 08:57:59 -0700

    wip

--- plugin.js
+++ plugin.js
@@ -3,6 +3,7 @@ exports.for = function (API) {
 
 	const SSH = require("./ssh").for(API);
 	const RSYNC = require("./rsync").for(API);
+//	org.sourcemint.genesis.lib
 
 
 	// TODO: This implementation should come from the VM plugin and we just call it here.
@@ -19,14 +20,14 @@ exports.for = function (API) {
                 commands.unshift('export DEBUG=' + (API.DEBUG?"1":""));
 	    	}
             commands.unshift('if [ -e ' + resolvedConfig.env.PIO_BIN_DIRPATH + '/activate ]; then . ' + resolvedConfig.env.PIO_BIN_DIRPATH + '/activate; fi');
-			API.console.verbose(("Running remote commands '" + commands.join("; ") + "'.").magenta);
+//			API.console.verbose(("Running remote commands '" + commands.join("; ") + "'.").magenta);
             return SSH.runRemoteCommands({
 				targetUser: resolvedConfig.ssh.user,
 	            targetHostname: resolvedConfig.ssh.host,
 	            commands: commands,
 	            workingDirectory: "~/",
 	            keyPath: resolvedConfig.ssh.privateKeyPath,
-	            timeout: 30
+	            timeout: 60 * 30	// 30 minutes
             });
 	    }
 
@@ -66,8 +67,11 @@ exports.for = function (API) {
         	}
         	resolvedConfig.env.PIO_HOME = resolvedConfig.env.PIO_HOME || "$HOME/io.pinf";
         	resolvedConfig.env.PIO_BIN_DIRPATH = resolvedConfig.env.PIO_BIN_DIRPATH || (resolvedConfig.env.PIO_HOME + "/bin");
-        	resolvedConfig.env.PIO_SERVICES_DIRPATH = resolvedConfig.env.PIO_SERVICES_DIRPATH || (resolvedConfig.env.PIO_HOME + "/services");
+        	resolvedConfig.env.PIO_LOG_DIRPATH = resolvedConfig.env.PIO_LOG_DIRPATH || (resolvedConfig.env.PIO_HOME + "/log");
+        	resolvedConfig.env.PIO_RUN_DIRPATH = resolvedConfig.env.PIO_RUN_DIRPATH || (resolvedConfig.env.PIO_HOME + "/run");
+        	resolvedConfig.env.PIO_DATA_DIRPATH = resolvedConfig.env.PIO_DATA_DIRPATH || (resolvedConfig.env.PIO_HOME + "/data");
 
+        	resolvedConfig.env.PIO_SERVICES_DIRPATH = resolvedConfig.env.PIO_SERVICES_DIRPATH || (resolvedConfig.env.PIO_HOME + "/services");
 
 			resolvedConfig.status = (previousResolvedConfig && previousResolvedConfig.status) || "unknown";
 
@@ -130,10 +134,10 @@ exports.for = function (API) {
                         // Make sure some default directories exist
                         'if [ ! -d ' + resolvedConfig.env.PIO_BIN_DIRPATH + ' ]; then mkdir ' + resolvedConfig.env.PIO_BIN_DIRPATH + '; fi',
                         'if [ ! -d ' + resolvedConfig.env.PIO_HOME + '/cache ]; then mkdir ' + resolvedConfig.env.PIO_HOME + '/cache; fi',
-                        'if [ ! -d ' + resolvedConfig.env.PIO_HOME + '/data ]; then mkdir ' + resolvedConfig.env.PIO_HOME + '/data; fi',
+                        'if [ ! -d ' + resolvedConfig.env.PIO_DATA_DIRPATH + ' ]; then mkdir ' + resolvedConfig.env.PIO_DATA_DIRPATH + '; fi',
                         'if [ ! -d ' + resolvedConfig.env.PIO_HOME + '/tmp ]; then mkdir ' + resolvedConfig.env.PIO_HOME + '/tmp; fi',
-                        'if [ ! -d ' + resolvedConfig.env.PIO_HOME + '/log ]; then mkdir ' + resolvedConfig.env.PIO_HOME + '/log; fi',
-                        'if [ ! -d ' + resolvedConfig.env.PIO_HOME + '/run ]; then mkdir ' + resolvedConfig.env.PIO_HOME + '/run; fi',
+                        'if [ ! -d ' + resolvedConfig.env.PIO_LOG_DIRPATH + ' ]; then mkdir ' + resolvedConfig.env.PIO_LOG_DIRPATH + '; fi',
+                        'if [ ! -d ' + resolvedConfig.env.PIO_RUN_DIRPATH + ' ]; then mkdir ' + resolvedConfig.env.PIO_RUN_DIRPATH + '; fi',
                         'if [ ! -d ' + resolvedConfig.env.PIO_SERVICES_DIRPATH + ' ]; then mkdir ' + resolvedConfig.env.PIO_SERVICES_DIRPATH + '; fi',
                         'if [ ! -f "' + resolvedConfig.env.PIO_BIN_DIRPATH + '/activate" ]; then',
                         '  echo "#!/bin/sh -e\nexport PATH=' + resolvedConfig.env.PIO_BIN_DIRPATH + ':$PATH" > ' + resolvedConfig.env.PIO_BIN_DIRPATH + '/activate',
@@ -188,69 +192,80 @@ console.log("TURN pio.deploy", resolvedConfig);
 	        ]);
 	    }
 
-	    function uploadService (sourcePath, serviceId) {
+	    function prepareService (sourcePath, serviceId, options) {
+
+	    	API.console.verbose("Prepare service '" + serviceId + "' from source path: " + sourcePath);
+
+	    	return API.Q.resolve(sourcePath);
+	    }
+
+
+	    function uploadService (sourcePath, serviceId, options) {
 
 			var servicePath = resolvedConfig.env.PIO_SERVICES_DIRPATH + "/" + serviceId;
 			var serviceSyncPath = servicePath + "/sync";
 
-			return API.Q.nbind(API.PACKAGE.fromFile, API.PACKAGE)(API.PATH.join(sourcePath, "package.json"), {
-				env: resolvedConfig.env
-			}).then(function (descriptor) {
-				return ensureServicePrerequisites(servicePath).then(function () {
-					return api.uploadFiles(sourcePath, serviceSyncPath).then(function () {
-
-						var pioConfig = descriptor.configForLocator(API.LOCATOR.fromConfigId("pio.pinf.io/0"));
-						function normalize (pioConfig) {
-							// TODO: This should already be normalized by 'configForLocator'.
-							if (!pioConfig.on) pioConfig.on = {};
-							if (!pioConfig.on.postdeploy) {
-								pioConfig.on.postdeploy = "io-pinf-pio-postdeploy";
-							}
-							if (/^\./.test(pioConfig.on.postdeploy)) {
-								pioConfig.on.postdeploy = serviceSyncPath + "/" + pioConfig.on.postdeploy;
-							}
-							return pioConfig;
-						}
-						pioConfig = normalize(JSON.parse(JSON.stringify(pioConfig || {})));
-
-						return api.runRemoteCommands([
-							'cd ' + servicePath,
-							pioConfig.on.postdeploy
-		                ], resolvedConfig.env);
+	    	return API.Q.spread([
+	    		// Load descriptor and call VM to ensure minimum prerequisites exist.
+	    		API.Q.nbind(API.PACKAGE.fromFile, API.PACKAGE)(API.PATH.join(sourcePath, "package.json"), {
+					env: resolvedConfig.env
+				}).then(function (descriptor) {
+					return ensureServicePrerequisites(servicePath).then(function () {
+						return descriptor;
 					});
+				}),
+				// Get the service ready for upload.
+				prepareService(sourcePath, serviceId, options)
+	    	], function (descriptor, sourcePath) {
+
+		    	API.console.verbose("Upload service '" + serviceId + "' to sync path: " + serviceSyncPath);
+
+				return api.uploadFiles(sourcePath, serviceSyncPath).then(function () {
+
+					var pioConfig = descriptor.configForLocator(API.LOCATOR.fromConfigId("pio.pinf.io/0"));
+					function normalize (pioConfig) {
+						// TODO: This should already be normalized by 'configForLocator'.
+						if (!pioConfig.on) pioConfig.on = {};
+						if (!pioConfig.on.postdeploy) {
+							pioConfig.on.postdeploy = "io-pinf-pio-postdeploy";
+						}
+						if (/^\./.test(pioConfig.on.postdeploy)) {
+							pioConfig.on.postdeploy = serviceSyncPath + "/" + pioConfig.on.postdeploy;
+						}
+						return pioConfig;
+					}
+					pioConfig = normalize(JSON.parse(JSON.stringify(pioConfig || {})));
+
+					return api.runRemoteCommands([
+						'cd ' + servicePath,
+						'echo -e "' + JSON.stringify(options.programDescriptor || {}, null, 4).replace(/"/g, '\\"') + '" > "' + serviceSyncPath + '/package.program.json"',
+						'export PIO_SERVICE_HOME=' + servicePath,
+						'export PIO_SERVICE_ID_SAFE=' + serviceId.replace(/\./g, "-"),
+						'export PIO_SERVICE_LOG_BASEPATH=' + resolvedConfig.env.PIO_LOG_DIRPATH + "/" + serviceId.replace(/\./g, "-"),
+						'export PIO_SERVICE_RUN_BASEPATH=' + resolvedConfig.env.PIO_RUN_DIRPATH + "/" + serviceId.replace(/\./g, "-"),
+						'export PIO_SERVICE_DATA_BASEPATH=' + resolvedConfig.env.PIO_DATA_DIRPATH + "/" + serviceId.replace(/\./g, "-"),
+						pioConfig.on.postdeploy
+	                ], resolvedConfig.env);
 				});
-			});
+	    	});
 	    }
 
-
-		function uploadBashOrigin () {
-			return uploadService(
-				API.PATH.dirname(require.resolve("bash.origin/package.json")),
-				"bash.origin"
-			);
-		}
-
-		function uploadPostdeploy () {
-			return uploadService(
-				API.PATH.dirname(require.resolve("io.pinf.pio.postdeploy/package.json")),
-				"io.pinf.postdeploy"
-			);
-		}
-
-		function uploadProxy () {
-			return uploadService(
-				API.PATH.dirname(require.resolve("io.pinf.proxy/package.json")),
-				"io.pinf.proxy"
-			);
-		}
-
-		return uploadBashOrigin().then(function () {
-
-			return uploadPostdeploy().then(function () {
-
-				return uploadProxy();
-			});
-		});
+	    var done = API.Q.resolve();
+	    API.DESCRIPTOR.sortObjByDepends(resolvedConfig.services).forEach(function (id) {
+	    	done = API.Q.when(done, function () {
+				return uploadService(
+					// TODO: Support more advanced locators.
+					API.PATH.dirname(require.resolve(resolvedConfig.services[id].location + "/package.json")),
+					resolvedConfig.services[id].location,
+					{
+						programDescriptor: {
+							config: resolvedConfig.services[id].config
+						}
+					}
+				);
+	    	});
+	    });
+	    return done;
 	}
 
 	return exports;
